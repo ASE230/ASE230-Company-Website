a695ab62a3075c48778f00779a177cec40ed9ac4
Kevin Muscara <muscarak1@nku.edu>
2023-09-15 11:42:24 -0400

    incorrect import, remove './' leading the file path

--- lib/display.php
+++ lib/display.php
@@ -1,6 +1,6 @@
 <!-- File for display-related functions -->
 <?php
-    // require_once('./csv.php');
+    require_once('csv.php');
+    require_once('txt.php');
     // require_once('./json.php');
-    // require_once('./txt.php');
 ?>
